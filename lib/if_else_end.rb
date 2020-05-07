76ee25df9ae71d5c532ef6982371983087f562bb
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-07 04:05:00 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,8 @@
 # Write your solution here
-chance_of_rain = 0.2
-
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack an umbrella!"
+current_time = Time.now
+current_time = current_time.to_i
+if current_time % 2 == 0
+  puts "Even!"
 else
-  puts "Stay home and read Hegel."
+  puts "Odd!"
 end
